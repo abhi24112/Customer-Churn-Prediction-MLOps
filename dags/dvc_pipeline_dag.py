809249bd483d1a5d184fcfccedf5240b6956de3f
abhi24112 <onlystudy24112002@gmail.com>
2026-05-06 17:25:15 +0530

    Add DVC pipeline DAG and example astronaut ETL DAG
    - Created a new DAG for the Customer Churn DVC pipeline in `dvc_pipeline_dag.py` that pulls data, runs the DVC pipeline, and pushes results.
    - Added an example DAG `exampledag.py` that retrieves and prints the names of astronauts currently in space using the Open Notify API.
    - Updated `workflow_step.txt` to clarify DVC and Airflow integration.
    - Modified `params.yaml` and `dvc.lock` to change the `min_child_weight` parameter for the model.
    - Added a `packages.txt` file to specify required packages.
    - Introduced a test suite for DAGs in `test_dag_example.py` to ensure proper tagging and retry settings.
    - Created an `.airflowignore` file to exclude unnecessary files from Airflow.

---
+++ dags/dvc_pipeline_dag.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from datetime import datetime
+
+# Operators
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id = "Customer_Churn_DVC_pipeline",
+    description = "Run full ML pipeline using DVC Daily",
+    start_date = datetime(2026,5,3),
+    schedule = "@daily",
+    catchup = False
+) as dag:
+    
+    # Pulling the latest data from DVC remote
+    dvc_pull = BashOperator(
+        task_id = "dvc_pull_data",
+        bash_command="cd /usr/local/airflow && git config --global --add safe.directory /usr/local/airflow && dvc pull"
+    )
+
+    # Run full pipeline
+    dvc_repro = BashOperator(
+        task_id = "run_dvc_pipeline",
+        bash_command="cd /usr/local/airflow && git config --global --add safe.directory /usr/local/airflow && dvc repro"
+    )
+
+    # Pushing results (model and metrics) to remote
+    dvc_push = BashOperator(
+        task_id = "dvc_push_result",
+        bash_command="cd /usr/local/airflow && git config --global --add safe.directory /usr/local/airflow && dvc push"
+    )
+
+    # Flow
+    dvc_pull >> dvc_repro >> dvc_push
+
+
+
+
+
+
+
